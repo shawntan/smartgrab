831c759cf9979af21f9f0036cf1adadf963abd02
shawntan <shawn@wtf.sg>
2010-07-07 17:35:42 +0800

    Shifted javascript files into view

--- app/controllers/bookmarklet_controller.rb
+++ app/controllers/bookmarklet_controller.rb
@@ -1,12 +1,17 @@
 class BookmarkletController < ApplicationController
+	def index
+		respond_to do |format|
+      format.js   # foo/bar.js.erb
+    end
+	end
 	def page
 		@page = Page.find_by_url(params[:page][:url]);
 		if @page 
-			render :json => @page.to_json
+			render :js => "#{params[:c]}(#{@page.to_json});"
 		else
 			@page = Page.new(params[:page])
 			if @page.save
-				render :json => @page.to_json
+				render :js => "#{params[:c]}(#{@page.to_json},true);"
 			end
 		end
 	end
