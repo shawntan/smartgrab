a470927690239977d8ddb6371657ff916bae17bc
shawn@wtf.sg <shawn@wtf.sg@aeb1d6df-4ce0-194b-fa34-65fafb13a8e6>
2010-07-15 06:05:58 +0000

    Bookmarklet: -Create new page URL
    git-svn-id: https://nus-parcels.googlecode.com/svn/trunk@11 aeb1d6df-4ce0-194b-fa34-65fafb13a8e6

--- app/controllers/bookmarklet_controller.rb
+++ app/controllers/bookmarklet_controller.rb
@@ -1,2 +1,13 @@
 class BookmarkletController < ApplicationController
+	def page
+		@page = Page.find_by_url(params[:page][:url]);
+		if @page 
+			render :json => @page.to_json
+		else
+			@page = Page.new(params[:page])
+			if @page.save
+				render :json => @page.to_json
+			end
+		end
+	end
 end
